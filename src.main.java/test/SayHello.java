774fd754390b80241318be962b77592920468c03
LeucotheaShi <1018746347@qq.com>
2016-11-28 11:10:27 +0800

    say hello
    Change-Id: I621d856b75e00b16b53de00c26753bcdd6267f25
    Signed-off-by: LeucotheaShi <1018746347@qq.com>

---
+++ src.main.java/test/SayHello.java
@@ -0,0 +1,35 @@
+package test;
+
+/**
+ * @ClassName:test.SayHello
+ * @Description: TODO
+ * @Date: Nov 28, 2016
+ * @Author: LeucotheaShi
+ */
+public class SayHello {
+
+	/**
+	 * 
+	 * @Description:
+	 * @Date:Nov 28, 2016 10:56:32 AM
+	 * @Author:LeucotheaShi
+	 */
+	public SayHello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @Title: main
+	 * @Description:
+	 * @param args
+	 * @return：void
+	 * @Date:Nov 28, 2016 10:56:32 AM
+	 * @Author:LeucotheaShi
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github!");
+
+	}
+
+}
